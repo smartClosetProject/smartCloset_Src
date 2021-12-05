14495e75f4afbeda744a64ee90e9387f29f1bf2c
jhch4 <minaislife7@naver.com>
2021-12-05 21:31:47 +0900

    orderVO 배송 정보 필드 추가. insert 쿼리 수정

--- smartClosetProject/src/test/java/com/spring/client/order/dao/OrderMapperTests.java
+++ smartClosetProject/src/test/java/com/spring/client/order/dao/OrderMapperTests.java
@@ -29,16 +29,22 @@ public class OrderMapperTests {
 //		log.info("result : " + vo);
 //	}
 //	
-//	@Test
-//	public void testInsertOrder() {
-//		OrderVO ovo = new OrderVO();
-//		ovo.setOrder_num(1234567890);
-//		ovo.setOrder_totalPayment(370000);
-//		ovo.setOrder_state("결제완료");
-//		ovo.setM_id("smartmember");
-//		
-//		orderDao.insertOrder(ovo);
-//	}
+	@Test
+	public void testInsertOrder() {
+		log.info("testInsertOrder 호출 성공");
+		
+		OrderVO ovo = new OrderVO();
+		ovo.setOrder_num(1234567890);
+		ovo.setOrder_totalPayment(370000);
+		ovo.setOrder_state("결제완료");
+		ovo.setM_id("smartmember");
+		ovo.setM_name("김자바");
+		ovo.setM_addr("서울시 중랑구 상봉동");
+		ovo.setM_phone("010-1234-4321");
+		ovo.setShipMessage("부재 시 문 앞에 놓아주세요.");
+		
+		orderDao.insertOrder(ovo);
+	}
 //	
 //	@Test
 //	public void testInsertOrderDetail() {
@@ -49,16 +55,16 @@ public class OrderMapperTests {
 //		orderDao.insertOrderDetail(odvo);
 //	}
 	
-	@Test
-	public void testUpdateMile() {
-		log.info("testUpdateMile 호출 성공");
-		
-		OrderVO ovo = new OrderVO();
-		ovo.setM_id("smartmember");
-		ovo.setM_mile(10000);
-		ovo.setM_mileApply(3000);
-		ovo.setM_mileAdd(3000);
-		
-		orderDao.updateMile(ovo);
-	}
+//	@Test
+//	public void testUpdateMile() {
+//		log.info("testUpdateMile 호출 성공");
+//		
+//		OrderVO ovo = new OrderVO();
+//		ovo.setM_id("smartmember");
+//		ovo.setM_mile(10000);
+//		ovo.setM_mileApply(3000);
+//		ovo.setM_mileAdd(3000);
+//		
+//		orderDao.updateMile(ovo);
+//	}
 }
